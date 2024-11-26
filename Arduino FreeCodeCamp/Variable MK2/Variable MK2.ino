f1681f2ad6ab582fe5f4f0a01637be09e6dafe8c
Jonathan Powell <jprophit@gmail.com>
2024-11-26 18:16:00 +0000

    Create Variable MK2.ino

---
+++ Arduino FreeCodeCamp/Variable MK2/Variable MK2.ino
@@ -0,0 +1,21 @@
+/*
+variables are used for storing nsimple numbers, this dose not include decimals
+*/
+
+
+
+int myVariable =6;
+int mySecondVariable =7;
+int myThridVariable;
+void setup() {
+    // put my setup code here, to run once:
+    Serial.begin(9600);
+    (myThirdVariable = myVariable+mySecondVariable;
+    Serial.println(myThirdVariable);
+    
+   
+}
+
+void loop() {
+    // put my setup code here, to run repeatedly:
+}
