b7617d2e5a3e4b7df3c79a3315d56bc07e50273f
Jonathan Powell <jprophit@gmail.com>
2024-11-26 17:58:13 +0000

    Create Modulo.ino

---
+++ Arduino FreeCodeCamp/Modulo/Modulo.ino
@@ -0,0 +1,19 @@
+/*
+modulo gives the value of the remainder number value from division
+*/
+
+
+
+int myVariable =6;
+void setup() {
+    // put my setup code here, to run once:
+    Serial.begin(9600);
+    myVariable = myVariable%2
+    Serial.println(myVariable;);
+    
+   
+}
+
+void loop() {
+    // put my setup code here, to run repeatedly:
+}
