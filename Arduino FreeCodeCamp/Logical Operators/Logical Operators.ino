bba34539b5e21dd1e0c10f3cdb18dd8a6b2df768
Jonathan Powell <jprophit@gmail.com>
2024-11-26 17:51:19 +0000

    Create Logical Operators.ino

---
+++ Arduino FreeCodeCamp/Logical Operators/Logical Operators.ino
@@ -0,0 +1,55 @@
+/*
+Logical operators are used when you want to take desicion in the code
+each oprand is applied to some codition, which maybe be true or flase
+This ia also a bollean value
+there are 3 logical operators:-
+1. and -  &&, 
+2. or -   ||, 
+3. not -  !, 
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  Serial.println("This lessons about && operator");
+  Serial.println("true and true is :");
+  Serial.println(true&&true);
+  Serial.println("ture and flase is :");
+  Serial.println(true&&flase);
+  Serial.println("false and true is :");
+  Serial.println(false&&true);
+  Serial.println("false and false is :");
+  Serial.println(false&&false);
+
+  Serial.println
+  Serial.println
+  Serial.println
+
+  Serial.println("This lessons about || operator");
+  Serial.println("true or true is :");
+  Serial.println(true||true);
+  Serial.println("ture or flase is :");
+  Serial.println(true||flase);
+  Serial.println("false or true is :");
+  Serial.println(false||true);
+  Serial.println("false or false is :");
+  Serial.println(false||false);
+
+  Serial.println
+  Serial.println
+  Serial.println
+
+  Serial.println("This lessons about ! operator");
+  Serial.println("The not of true is :");
+  Serial.println(!true);
+  Serial.println("The not of flase is :");
+  Serial.println(!flase);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
