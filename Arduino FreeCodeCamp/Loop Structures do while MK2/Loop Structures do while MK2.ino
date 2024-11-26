3a4ac737bcc77f285e78aa43cca1806a7cbab503
Jonathan Powell <jprophit@gmail.com>
2024-11-26 17:53:15 +0000

    Create Loop Structures do while MK2.ino

---
+++ Arduino FreeCodeCamp/Loop Structures do while MK2/Loop Structures do while MK2.ino
@@ -0,0 +1,44 @@
+/*
+Loop Structures.  viod loop is a given default loop structure.  but you may need something to repeat itself.
+for this we have 3 loop structures
+
+1. for
+2. while
+3. do while
+
+the loop will continously execute until it has met the requirements of the statement.
+it will then end the loop moving onto the next statement and then exit 
+
+do {  //condition (1)
+  //statement block
+} while (condition);  //condition (2 & 3)
+
+
+
+exit controlled loop
+used when we don't know how many times an action has to be repeated but it should run at least once
+
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  static int testVariable;
+
+  do {
+    Serial.println("This will get printed at least once");
+    delay(500);
+  } while (testVariable > 5);
+
+  testVariable++;
+  Serial.println(testVariable);
+  delay(500);
+}
